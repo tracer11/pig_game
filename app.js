6c98078ca20358ea24f36754bbd74a71e0283f11
tracer11 <karolgurba22@gmail.com>
2017-10-24 18:27:41 -0500

    added hold button function

--- app.js
+++ app.js
@@ -40,17 +40,34 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-      activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+      nextPlayer();
+    }
+
+
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+  scores[activePlayer] += roundScore;
+
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  nextPlayer();
+
+
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
       roundScore = 0;
 
       document.getElementById('current-0').textContent = 0;
       document.getElementById('current-1').textContent = 0;
       document.querySelector('.player-0-panel').classList.toggle('active');
       document.querySelector('.player-1-panel').classList.toggle('active');
-    }
 
-
-});
+      document.querySelector('.dice').style.display = 'none';
+}
 
 
 
