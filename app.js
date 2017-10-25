a10d2e9245c5c905fda3fd0564c10dfebf662245
tracer11 <karolgurba22@gmail.com>
2017-10-25 11:52:36 -0500

    finished making the new game function

--- app.js
+++ app.js
@@ -13,18 +13,11 @@ var scores, roundScore, activePlayer;
 
 
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+newGame();
 
 
 
-document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     // 1 Random Number
@@ -72,8 +65,34 @@ function nextPlayer() {
       document.getElementById('current-1').textContent = 0;
       document.querySelector('.player-0-panel').classList.toggle('active');
       document.querySelector('.player-1-panel').classList.toggle('active');
+}
 
-      document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-new').addEventListener('click', newGame);
+
+function newGame(){
+  scores = [0,0];
+  activePlayer = 0;
+  roundScore = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = '0'
+  document.getElementById('score-1').textContent = '0'
+  document.getElementById('current-0').textContent = '0'
+  document.getElementById('current-1').textContent = '0'
+
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+
+  document.querySelector('.player-0-panel').classList.add('active');
+
+
 }
 
 
@@ -85,10 +104,3 @@ function nextPlayer() {
 
 
 
-
-// document.querySelector('#current-' + activePlayer).textContent = dice;
-
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-// var x = document.querySelector('#score-0').textContent;
-// console.log(x);
